d00a83035a846e577b90b67ef892a31465c6dfde
Janicek2 <bjanicki82@gmail.com>
2018-06-04 10:18:07 +0200

    Add info function

--- program.js
+++ program.js
@@ -1,3 +1,4 @@
+var os = require('os');
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
@@ -16,8 +17,29 @@ process.stdin.on('readable', function() {
         process.stdout.write('Wychodzimy!\n');
         process.exit();
         break;
+      case '/info':
+        getOSinfo();
+        break;
       default:
         process.stderr.write('Złe instrukcje!\n');
     }
+  }
+  function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', userInfo.username);
+    console.log('Home dir:', userInfo.homedir);
   }
 });
